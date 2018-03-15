90cc334e4ce944d442c4f4d78f9a7a9b1b659c19
m305-13\w401 <aierzhiguang2@gmail.com>
2018-03-15 18:28:03 +0800

    很好玩

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,8 +9,51 @@ namespace ConsoleApplication1
     class Program
     {
         static void Main(string[] args)
-        {    
-            
+        {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine("請誠實地輸入身高");
+                double height = double.Parse(Console.ReadLine());
+                Console.WriteLine("請誠實地輸入體重");
+                double weight = double.Parse(Console.ReadLine());
+                double BMI = weight*10000/(height*height);
+                
+                if (BMI < 18.5)
+                {
+                    Console.WriteLine("體重過輕");
+                    Console.Beep();
+                    Console.BackgroundColor = ConsoleColor.Red;
+                }
+                else if (BMI>=18.5  && BMI < 24)
+                {
+                    Console.WriteLine("恭喜你，非常正常^^");
+                    Console.BackgroundColor = ConsoleColor.Green;
+                }
+                else if (BMI>=24 && BMI < 27)
+                {
+                    Console.WriteLine("你有點過重哦");
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Beep();
+                }
+                else if ( BMI>=27 && BMI < 30)
+                {
+                    Console.WriteLine("輕度肥胖");
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Beep();
+                }
+                else if (BMI>=30 && BMI < 35)
+                {
+                    Console.WriteLine("中度肥胖");
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Beep();
+                }
+                else
+                {
+                    Console.WriteLine("重度肥胖");
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Beep();
+                }
+            Console.WriteLine("你的BMI數值是"+BMI.ToString());
+            Console.ReadLine();
+            }
         }
     }
-}
