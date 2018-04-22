109e3a36d0a7440fb2e7b34bc805ff06d5ee5614
stupic999 <37150488+stupic999@users.noreply.github.com>
2018-04-22 19:24:39 +0800

    刪減了無用的文件夾與註解

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,50 +10,69 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.WriteLine("請誠實地輸入身高");
-                double height = double.Parse(Console.ReadLine());
-                Console.WriteLine("請誠實地輸入體重");
-                double weight = double.Parse(Console.ReadLine());
-                double BMI = weight*10000/(height*height);
-                
-                if (BMI < 18.5)
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("請誠實地輸入身高");
+
+            // 宣告身高的變數 
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("請誠實地輸入體重");
+  
+            // 宣告體重的變數 
+            double weight = double.Parse(Console.ReadLine());
+
+            // 宣告BMI的公式 
+            double BMI = weight*10000/(height*height);
+
+            if (BMI < 18.5) 
                 {
-                    Console.WriteLine("體重過輕");
-                    Console.Beep();
-                    Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("體重過輕");
+
+                // 發出“嗶”的聲響
+                Console.Beep();
+                ·
+                Console.BackgroundColor = ConsoleColor.Red;  
                 }
-                else if (BMI>=18.5  && BMI < 24)
+
+            else if (BMI>=18.5  && BMI < 24)
                 {
-                    Console.WriteLine("恭喜你，非常正常^^");
-                    Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("恭喜你，非常正常^^");
+
+                Console.BackgroundColor = ConsoleColor.Green;
                 }
-                else if (BMI>=24 && BMI < 27)
+
+            else if (BMI>=24 && BMI < 27)
                 {
-                    Console.WriteLine("你有點過重哦");
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.Beep();
+                Console.WriteLine("你有點過重哦");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Beep();
                 }
-                else if ( BMI>=27 && BMI < 30)
+
+            else if ( BMI>=27 && BMI < 30)
+                { 
+                Console.WriteLine("輕度肥胖");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Beep();
+                }
+
+            else if (BMI>=30 && BMI < 35)
                 {
-                    Console.WriteLine("輕度肥胖");
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.Beep();
+                Console.WriteLine("中度肥胖");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Beep();
                 }
-                else if (BMI>=30 && BMI < 35)
-                {
-                    Console.WriteLine("中度肥胖");
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.Beep();
+
+            else
+            {
+                Console.WriteLine("重度肥胖");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Beep();
                 }
-                else
-                {
-                    Console.WriteLine("重度肥胖");
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.Beep();
-                }
+
             Console.WriteLine("你的BMI數值是"+BMI.ToString());
-            Console.ReadLine();
+
+            // 按任意按鈕即可退出 
+            Console.ReadKey();
             }
         }
     }
